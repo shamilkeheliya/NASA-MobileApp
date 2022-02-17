2dfc15735118c9d2a41b20b614eb655f7cd13f07
shamilkeheliya <shamilkeheliya@gmail.com>
2022-02-18 00:09:24 +0530

    Update home_page.dart

--- nasa_mobileapp/lib/views/home_page.dart
+++ nasa_mobileapp/lib/views/home_page.dart
@@ -59,7 +59,7 @@ class _HomePageState extends State<HomePage> {
                             visible: searchKey != '',
                             child: IconButton(
                               onPressed: () {
-                                searchKeyTEC.clear;
+                                searchKeyTEC.clear();
                                 setState(() {
                                   searchKey = '';
                                 });
