e5ae3db6718e05fc77d003f132377ee58b35caf8
shamilkeheliya <shamilkeheliya@gmail.com>
2022-06-21 18:40:27 +0530

    Updated

--- Readme.md
+++ Readme.md
@@ -13,7 +13,6 @@ This mobile application was created for the NASA image library using Flutter.
 - [shared_preferences](https://pub.dev/packages/shared_preferences)
 - [provider](https://pub.dev/packages/provider)
 - [modal_progress_hud_nsn](https://pub.dev/packages/modal_progress_hud_nsn)
-- [expandable](https://pub.dev/packages/expandable)
 - [url_launcher](https://pub.dev/packages/url_launcher)
 - [cached_network_image](https://pub.dev/packages/cached_network_image)
 
--- lib/customWidgets/detailsCard.dart
+++ lib/customWidgets/detailsCard.dart
@@ -1,4 +1,3 @@
-import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:cached_network_image/cached_network_image.dart';
@@ -19,58 +18,54 @@ class CustomDetailCard extends StatelessWidget {
       elevation: 5,
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: ExpandablePanel(
-          theme: ExpandableThemeData(
-            iconColor: Theme.of(context).canvasColor,
-          ),
-          header: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Flexible(
-                child: Text(
-                  title,
-                  softWrap: true,
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context).canvasColor,
-                  ),
-                ),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: CachedNetworkImage(
-                      imageUrl: "http://via.placeholder.com/350x150",
-                      placeholder: (context, url) =>
-                          CircularProgressIndicator(),
-                      errorWidget: (context, url, error) => Icon(Icons.error),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      softWrap: true,
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).canvasColor,
+                      ),
                     ),
-                  ),
+                    const SizedBox(height: 5),
+                    Text(
+                      description,
+                      maxLines: 4,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        color: Theme.of(context).canvasColor,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-          collapsed: Text(
-            description,
-            softWrap: true,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-              color: Theme.of(context).canvasColor,
             ),
-          ),
-          expanded: Text(
-            description,
-            softWrap: true,
-            maxLines: 8,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-              color: Theme.of(context).canvasColor,
+            SizedBox(
+              height: 100,
+              width: 100,
+              child: Hero(
+                tag: 'heroImage',
+                child: CachedNetworkImage(
+                  imageUrl: image,
+                  placeholder: (context, url) => Transform.scale(
+                    scale: 0.3,
+                    child: const CircularProgressIndicator(),
+                  ),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                ),
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
